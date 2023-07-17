8dcbb6167ecde478d4939b00e8d67c8298c04f55
Hanjunn22 <115737263+Hanjunn22@users.noreply.github.com>
2023-07-17 19:48:58 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -0,0 +1,14 @@
+function solution(dot) {
+    const x = dot[0];
+    const y = dot[1];
+    
+    if (x > 0 && y > 0) {
+        return 1; // 제1사분면
+    } else if (x < 0 && y > 0) {
+        return 2; // 제2사분면
+    } else if (x < 0 && y < 0) {
+        return 3; // 제3사분면
+    } else {
+        return 4; // 제4사분면
+    }
+}
