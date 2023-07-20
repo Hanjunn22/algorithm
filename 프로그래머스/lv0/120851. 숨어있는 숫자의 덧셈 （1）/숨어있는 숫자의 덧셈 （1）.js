c182a57a6df19f910f4e05a6d44b15adbb3f4f38
Hanjunn22 <115737263+Hanjunn22@users.noreply.github.com>
2023-07-20 23:23:52 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -0,0 +1,10 @@
+function solution(my_string) {
+    let num = my_string.split("");
+    var answer = 0;
+    for(i=0; i<my_string.length; i++){
+        if(Number(num[i])){
+            answer += Number(num[i]);
+        }
+    }
+    return answer;
+}
