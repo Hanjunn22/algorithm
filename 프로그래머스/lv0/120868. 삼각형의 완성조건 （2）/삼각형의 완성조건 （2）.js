61ca8b87154a27e7f331120a3e8418474864a588
Hanjunn22 <115737263+Hanjunn22@users.noreply.github.com>
2023-08-16 01:43:54 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
+++ 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
@@ -1,3 +1,17 @@
 function solution(sides) {
-    return Math.min(...sides) * 2 - 1;
+  // 배열을 오름차순으로 정렬
+  sides.sort((a, b) => a - b);
+
+  // 가장 긴 변의 길이를 구합니다.
+  const longestSide = sides[2];
+
+  // 나머지 두 변의 길이 합이 가장 긴 변의 길이보다 작을 때까지
+  // 나머지 한 변의 길이를 증가시키며 개수를 구합니다.
+  let count = 0;
+  while (sides[0] + sides[1] <= longestSide) {
+    sides[1]++; // 두 번째로 긴 변의 길이를 증가
+    count++;
+  }
+
+  return count;
 }
