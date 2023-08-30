d13200ceec5c208a41a89f4f876af5578dde67b6
Hanjunn22 <115737263+Hanjunn22@users.noreply.github.com>
2023-08-30 19:08:28 +0900

    [unrated] Title: 꼬리 문자열, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.js
@@ -0,0 +1,11 @@
+function solution(str_list, ex) {
+    let result = "";
+
+    for (let str of str_list) {
+        if (!str.includes(ex)) {
+            result += str;
+        }
+    }
+
+    return result;
+}
