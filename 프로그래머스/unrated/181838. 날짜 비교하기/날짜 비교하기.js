9f337a4ccaab701921995352e5a289aa07d9808e
Hanjunn22 <115737263+Hanjunn22@users.noreply.github.com>
2023-09-13 00:44:32 +0900

    [unrated] Title: 날짜 비교하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
@@ -0,0 +1,11 @@
+function solution(date1, date2) {
+    var answer = 0;
+    for(let i = 0; i<=3; i++){
+        if(date1[i]<date2[i]){
+            answer = 1;
+        } else if(date1[i]>date2[i]){
+            break;
+        }
+    }
+    return answer;
+}
