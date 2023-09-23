794ee1acdd90fec18542f2d49f9b06987b952786
Hanjunn22 <115737263+Hanjunn22@users.noreply.github.com>
2023-09-23 15:12:41 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 2, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.js
@@ -0,0 +1,21 @@
+function solution(arr) {
+    let answer = 0;
+    let count = 0;
+    let beforeArr = [...arr];
+    
+    while (count !== arr.length) {
+        count = 0;
+        beforeArr = [...arr];
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 === 0 && arr[i] >= 50) {
+                arr[i] /= 2;
+            } else if (arr[i] % 2 !== 0 && arr[i] < 50) {
+                arr[i] = arr[i] * 2 + 1;
+            }
+            if (arr[i] === beforeArr[i]) count++;
+        }
+        answer++;
+    }
+
+    return answer - 1;
+}
