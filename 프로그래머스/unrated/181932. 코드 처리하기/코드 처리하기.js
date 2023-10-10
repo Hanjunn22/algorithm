a2678746b934083662fac7b893bd98db0124c49e
Hanjunn22 <115737263+Hanjunn22@users.noreply.github.com>
2023-10-10 15:24:29 +0900

    [level unrated] Title: 코드 처리하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.js
@@ -0,0 +1,14 @@
+function solution(code) {
+    let mode = 0;
+    let ret = "";
+
+    for (let idx = 0; idx < code.length; idx++) {
+        if (code[idx] === "1") {
+            mode = 1 - mode;
+        } else if ((mode === 0 && idx % 2 === 0) || (mode === 1 && idx % 2 !== 0)) {
+            ret += code[idx];
+        }
+    }
+
+    return ret === "" ? "EMPTY" : ret;
+}
