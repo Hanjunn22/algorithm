92e1b651179f06fcd023e5ca5ca0adaac193b351
Hanjunn22 <115737263+Hanjunn22@users.noreply.github.com>
2023-11-05 00:47:56 +0900

    [level 2] Title: 올바른 괄호, Time: 2.66 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,21 @@
+function solution(s) {
+    let answer = true;
+    let count = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === "(") {
+            count++;
+        } else {
+            count--;
+            if (count < 0) {
+                return false;
+            }
+        }
+    }
+
+    if (count !== 0) {
+        answer = false;
+    }
+
+    return answer;
+}
