2547279a354347102bd0d91f5916262161d9c648
Hanjunn22 <115737263+Hanjunn22@users.noreply.github.com>
2023-11-11 19:44:50 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.13 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -0,0 +1,22 @@
+function gcd(a, b) {
+  while (b !== 0) {
+    var r = a % b;
+    a = b;
+    b = r;
+  }
+  return a;
+}
+
+function lcm(a, b) {
+  return a * b / gcd(a, b);
+}
+
+function solution(arr) {
+  var answer = arr[0];
+  
+  for (var i = 1; i < arr.length; i++) {
+    answer = lcm(answer, arr[i]);
+  }
+  
+  return answer;
+}
