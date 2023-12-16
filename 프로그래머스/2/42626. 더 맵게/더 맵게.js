49e861379cae5e00a66b2bcdd79a3ecb76efe4c4
조한준 <115737263+Hanjunn22@users.noreply.github.com>
2023-12-16 23:15:57 +0900

    [level 2] Title: 더 맵게, Time: 415.71 ms, Memory: 92 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.js
@@ -0,0 +1,85 @@
+class MinHeap {
+    constructor() {
+        this.heap = [];
+    }
+
+    insert(value) {
+        this.heap.push(value);
+        this.bubbleUp();
+    }
+
+    bubbleUp() {
+        let index = this.heap.length - 1;
+        while (index > 0) {
+            let element = this.heap[index];
+            let parentIndex = Math.floor((index - 1) / 2);
+            let parent = this.heap[parentIndex];
+
+            if (parent <= element) break;
+            this.heap[index] = parent;
+            this.heap[parentIndex] = element;
+            index = parentIndex;
+        }
+    }
+
+    extractMin() {
+        const min = this.heap[0];
+        const last = this.heap.pop();
+        if (this.heap.length > 0) {
+            this.heap[0] = last;
+            this.sinkDown();
+        }
+        return min;
+    }
+
+    sinkDown() {
+        let index = 0;
+        const length = this.heap.length;
+        const element = this.heap[0];
+
+        while (true) {
+            let leftChildIndex = 2 * index + 1;
+            let rightChildIndex = 2 * index + 2;
+            let leftChild, rightChild;
+            let swap = null;
+
+            if (leftChildIndex < length) {
+                leftChild = this.heap[leftChildIndex];
+                if (leftChild < element) {
+                    swap = leftChildIndex;
+                }
+            }
+            if (rightChildIndex < length) {
+                rightChild = this.heap[rightChildIndex];
+                if (
+                    (swap === null && rightChild < element) || 
+                    (swap !== null && rightChild < leftChild)
+                ) {
+                    swap = rightChildIndex;
+                }
+            }
+
+            if (swap === null) break;
+            this.heap[index] = this.heap[swap];
+            this.heap[swap] = element;
+            index = swap;
+        }
+    }
+}
+
+function solution(scoville, K) {
+    let mixCount = 0;
+    const minHeap = new MinHeap();
+    scoville.forEach(item => minHeap.insert(item));
+
+    while (minHeap.heap.length > 1 && minHeap.heap[0] < K) {
+        const firstMin = minHeap.extractMin();
+        const secondMin = minHeap.extractMin();
+        const mixed = firstMin + secondMin * 2;
+        minHeap.insert(mixed);
+        mixCount++;
+    }
+
+    if (minHeap.heap[0] < K) return -1;
+    return mixCount;
+}
