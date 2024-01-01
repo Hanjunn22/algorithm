58adfd87ba8dfdc7cc7a521886e27365f4272d7e
조한준 <115737263+Hanjunn22@users.noreply.github.com>
2024-01-01 23:03:14 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.53 ms, Memory: 37.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -0,0 +1,26 @@
+function solution(maps) {
+    const n = maps.length;
+    const m = maps[0].length;
+    const dx = [1, -1, 0, 0];
+    const dy = [0, 0, 1, -1];
+
+    const queue = [[0, 0]];
+
+    while (queue.length > 0) {
+        const [x, y] = queue.shift();
+
+        for (let i = 0; i < 4; i++) {
+            const nx = x + dx[i];
+            const ny = y + dy[i];
+
+            if (nx >= 0 && ny >= 0 && nx < n && ny < m && maps[nx][ny] === 1) {
+                maps[nx][ny] = maps[x][y] + 1;
+                queue.push([nx, ny]);
+            }
+        }
+    }
+
+    const answer = maps[n - 1][m - 1];
+    return answer === 1 ? -1 : answer;
+}
+
